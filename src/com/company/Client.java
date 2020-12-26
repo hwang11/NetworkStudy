f9fcb6cff8f9309d866ca8267fce86cf4fb1b635
황선아 <tjsdk2769@gmail.com>
2020-12-26 21:51:11 +0900

    Add Server-Client Send String

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,11 +1,22 @@
 package com.company;
 
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("127.0.0.1",1000);
         System.out.println("서버와 접속됨.");
+
+        InputStream is = socket.getInputStream();
+        DataInputStream dis = new DataInputStream(is);
+        String str = dis.readUTF();
+        System.out.println(str);
+
+        is.close();
+        socket.close();
+        dis.close();
     }
 }
--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -1,12 +1,15 @@
 package com.company;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
         int port = 1000;
+        String str = new String(args[0]);
 
         ServerSocket ssk = new ServerSocket(port);
         System.out.println("접속 대기중");
@@ -15,6 +18,16 @@ public class Server {
             Socket sock = ssk.accept();
             System.out.println("사용자 접속 했습니다");
             System.out.println("Client ip :"+ sock.getInetAddress());
+
+            OutputStream ous = sock.getOutputStream();
+            DataOutputStream dous = new DataOutputStream(ous);
+
+            dous.writeUTF(str);
+            dous.flush();
+            dous.close();
+            ous.close();
+            sock.close();
+
         }
 
 
