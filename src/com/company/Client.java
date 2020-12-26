19da33cad16eba80771dd2d1c72ee3afbaf8c57e
황선아 <tjsdk2769@gmail.com>
2020-12-26 20:29:28 +0900

    Add Client

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,7 +1,11 @@
 package com.company;
 
-public class Client {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.Socket;
 
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("127.0.0.1",1000);
+        System.out.println("서버와 접속됨.");
     }
 }
